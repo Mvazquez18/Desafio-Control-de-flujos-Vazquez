555fa50761c0194078c3fbbe6abd9a4eff1a7c44
Mvazquez18 <martinvazquez095@gmail.com>
2021-09-06 23:49:00 -0300

    Desafio 3 Vazquez

--- js/01-app.js
+++ js/01-app.js
@@ -1,8 +1,44 @@
-let numero = parseInt (prompt ("Ingrese el valor del producto para saber si su envío tendrá costo o no"))
-const esMayor= numero > 3000
+let nombre =""
+let contar = 0;
+let razon=""
+let puntaje =""
+alert ("Enumere las películas/serios que vio últimamente. Escriba su opinión y un puntaje.");
 
-if (esMayor) {
-    alert ("Su envío será gratis") }
-else {
-    alert ("Su envío tendrá un costo de $500")
+while (nombre =" ") {
+ 
+    
+    nombre =prompt ("Introduzca el nombre de la película");
+    razon = razon.toUpperCase();
+
+    puntaje= prompt ("¿Que puntaje le pondría?");
+    
+    
+    razon= prompt ("Introduzca una razón")
+    razon = razon.toLowerCase();
+
+    if (puntaje >=7) {
+        alert ("Buenisimo, la pondremos en nuestra base de datos para recomendarla.")
+    }
+
+    if (puntaje <=6) {
+        alert ("Una peli de domingo, ¿no?")
+       
+    }
+
+    if (puntaje <=4) {
+        alert ("Lo bueno del cine es que tienen muchas películas, ya vas a encontrar alguna buena")
+       
+    }
+
+     if (puntaje <=1) {
+        alert ("¿Pero puede ser de esas que son tan malas que son buenas? Te recomiendo The Room (2003), es un buen ejemplo de esto")
+       
+    }
+
+    console.log ("La pelicula es " + nombre+ "." + " La recomiendo porque " + razon + ". Mi puntaje es " + puntaje);
+    i= nombre; continue;
+    
+    alert ("Muchas gracias")
+
+
 }
